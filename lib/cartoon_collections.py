64cdff3ea9a24cd347ebbdea76d8abbb2b239c42
Joy <joyglory213@gmail.com>
2023-11-27 21:41:22 +0300

    Completed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, name in enumerate(dwarves, start=1):
+        print(f"{i}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls=["earth", "wind", "fire", "water", "heart"]):
+    capitalize_calls = [calls.capitalize() + '!' for calls in planeteer_calls]
+    return capitalize_calls
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+
+def find_the_cheese(strings):
+    soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+
+    for string in strings:
+        if string.lower() in soup:
+            return string
+
+    return None
